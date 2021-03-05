d4afef41e79f71f174fe99cf2ea6f19cd142cde6
Andrew <drew073091@aol.com>
2021-03-04 21:17:38 -0500

    Added coding challenge 4 folder and rigis data
    Uses user input to set the workspace of an arcpy script and then takes clips the watersheds in Rhode Island to the Rhode Island state boundary.  There is an output folder to use as the workspace if desired.

---
+++ Coding_Challenge_4/Andrew_Million_NRS_528_Coding_Challenge_4.py
@@ -0,0 +1,18 @@
+import arcpy
+
+# workspace
+print("Please enter the file path to be used as your workspace")
+file_path = input()    # requires user input to set the file path that will be used in the workspace
+arcpy.env.workspace = file_path  # sets the workspace
+arcpy.env.scratchWorkspace = file_path  # sets the scratch workspace
+print("The workspace is now set to:")
+print(file_path)   # informs you that the workspace was set
+
+# tool variables
+in_features = 'Watershed_Boundary_Rhode_Island.shp'  # input for the clip tool
+clip_features = 'Rhode_Island_state_boundary.shp'    # feature to be used to clip the input
+out_feature_class = 'Rhode_Island_Watersheds.shp'    # output data from the clip tool
+# cluster_tolerance = '#'  # optional so leaving blank
+
+# tool
+arcpy.Clip_analysis(in_features, clip_features, out_feature_class)  # will clip the in_features to the shape of the clip_features
